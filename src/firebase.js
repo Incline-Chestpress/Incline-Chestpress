c6089554950b15b323fb6b131e5c92394d7d7d76
sh613 <sh.427613@docomo.ne.jp>
2021-06-05 16:32:15 +0900

    firebaseを設定

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVfr9fEtbManRw1XJxSnKQkf616hiUbVQ",
+  authDomain: "incline-chestpress.firebaseapp.com",
+  projectId: "incline-chestpress",
+  storageBucket: "incline-chestpress.appspot.com",
+  messagingSenderId: "737374460847",
+  appId: "1:737374460847:web:6b85fccf26b377c84b2d5a",
+  measurementId: "G-K32RKEGHNB",
+}
+
+firebase.initializeApp(firebaseConfig)
