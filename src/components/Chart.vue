1806128878d3e9c5d6fd177b9f9d10e6131f07bc
sh613 <sh.427613@docomo.ne.jp>
2021-06-20 18:33:09 +0900

    chart.js加

---
+++ src/components/Chart.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <input type="text" v-model="inputValue" />
+    <button v-on:click="addData">追加</button>
+
+    <p>{{ inputValue }}</p>
+    <Chart :chartData="chartItems" :options="chartOptions" />
+  </div>
+</template>
+
+<script>
+import Chart from "@/components/ChartBox.js"
+
+export default {
+  components: {
+    Chart,
+  },
+  data() {
+    return {
+      inputValue: "",
+      chartItems: {
+        labels: [
+          "12月",
+          "1月",
+          "2月",
+          "3月",
+          "4月",
+          "5月",
+          "6月",
+          "7月",
+          "8月",
+          "9月",
+          "10月",
+          "11月",
+        ],
+        datasets: [
+          {
+            label: "月ごとの点数",
+            data: [2, 2, 2, 2, 4, 5, 6, 7],
+            backgroundColor: "lightblue",
+            tension: 0,
+            fill: false,
+          },
+        ],
+      },
+      // 後程オプションを記述
+
+      chartOptions: {
+        maintainAspectRatio: false,
+        fill: false,
+        scales: {
+          xAxes: [
+            {
+              display: true,
+              // X軸グリッド非表示
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+          // Y軸
+          yAxes: [
+            {
+              display: true,
+              position: "right",
+              ticks: {
+                // 0から始まる
+                beginAtZero: true,
+                // 最大5目盛
+                maxTicksLimit: 5,
+              },
+            },
+          ],
+        },
+      },
+    }
+  },
+
+  methods: {
+    addData() {
+      let adddata = this.inputValue
+      this.datasets[0].data.push(adddata)
+      this.inputValue = ""
+      return this.datasets.data
+    },
+  },
+}
+</script>
--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -2,6 +2,7 @@
   <div>
     <TopBar />
     <Aside />
+    <Chart />
     <TodoBox />
   </div>
 </template>
@@ -9,12 +10,14 @@
 import TopBar from "@/components/TopBar.vue"
 import TodoBox from "@/components/TodoBox.vue"
 import Aside from "@/components/Aside.vue"
+import Chart from "@/components/Chart.vue"
 
 export default {
   components: {
     TodoBox,
     Aside,
     TopBar,
+    Chart,
   },
 }
 </script>
